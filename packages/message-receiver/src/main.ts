6356c07be519dbf822a2311d8b5afda2cef0fc47
Andy Gallagher <fredex42@gmail.com>
2023-04-20 17:10:29 +0100

    new lambda to demonstrate receiving SNS messages

--- packages/message-receiver/src/main.ts
+++ packages/message-receiver/src/main.ts
@@ -1,1 +1,27 @@
-console.log('Hello World');
+import {SNSHandler, SNSMessage} from "aws-lambda";
+
+interface EventShape {
+  source?: string;
+  content: string;
+}
+
+function safeParse(source: SNSMessage):EventShape {
+  try {
+    return JSON.parse(source.Message) as EventShape;
+  } catch(err) {
+    console.error(`Could not parse '${source.Message}' as EventShape: `, err);
+    return {
+      source: "internal",
+      content: "could not parse this message :("
+    }
+  }
+}
+
+export const handler:SNSHandler = (event) => {
+  console.log(`Received ${event.Records.length} events`);
+  event.Records.forEach((evt, ctr)=>{
+    const content = safeParse(evt.Sns);
+    console.log(`Message ${ctr}: ${evt.Sns.TopicArn} from ${content.source} at ${evt.Sns.Timestamp}`);
+    console.log(content.content);
+  })
+}
