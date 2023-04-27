4a79eeaef08ceecd78dfa50989fdc1718dc63254
Andy Gallagher <fredex42@gmail.com>
2023-04-27 13:12:27 +0100

    refactor code inline with slides

--- packages/message-receiver/src/main.ts
+++ packages/message-receiver/src/main.ts
@@ -5,26 +5,11 @@ interface EventShape {
   content: string;
 }
 
-function safeParse(source: SNSMessage):EventShape {
-  try {
-    return JSON.parse(source.Message) as EventShape;
-  } catch(err) {
-    console.error(`Could not parse '${source.Message}' as EventShape: `, err);
-    return {
-      source: "internal",
-      content: "could not parse this message :("
-    }
-  }
-}
-
 export const handler:SNSHandler = (event) => {
   console.log(`Received ${event.Records.length} events`);
   event.Records.forEach((evt, ctr)=>{
-    const content = safeParse(evt.Sns);
+    const content = JSON.parse(evt.Sns.Message) as EventShape;
     console.log(`Message ${ctr}: ${evt.Sns.TopicArn} from ${content.source} at ${evt.Sns.Timestamp}`);
     console.log(content.content);
   });
-  if(Math.random()>0.5) {
-    throw "Oooh crumbs!";
-  }
 }
